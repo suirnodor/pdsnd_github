308fc9fbcccccb233356fe13d392b823d9b5ae85
suirnodor <suirnodor@gmail.com>
2023-08-07 11:13:48 +0200

    comment in Spanish of the function time-stats

--- bikeshare.py
+++ bikeshare.py
@@ -96,7 +96,13 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+
+    Muestra estadísticas sobre los momentos de viaje más frecuentes.
+    
+    Args:
+        df (DataFrame): El DataFrame filtrado de datos de bicicletas compartidas.
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
